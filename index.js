f7967cd1b2415fd224fda4ea36ad47b00033bc59
vicken <vicken.liu@gmail.com>
2016-04-04 11:49:37 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
+var show = require('./views/show.hbs')
+var $ = require('jquery')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+xhr.get('http://localhost:3000/cats', function(err, data) {
   if (err) console.log(err) // do something
-
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+  var name = 'json',
+      returnData = JSON.parse(data.body)
+/*  returnData.forEach(function(obj){
+    console.log(obj)
+  })*/
+  console.log(returnData);
+  $('body').html( example( {cats: returnData.cats} ) );
 })
 
+
+  $('body').on('click','.showCat',function(e){
+    e.preventDefault();
+    var cat = e.target,
+        id=$(cat).data('id');
+        console.log('id, cat', id, cat)
+    xhr.get('http://localhost:3000/cats/'+id, function(err, data) {
+      if (err) console.log(err) // do something
+
+      var  returnData = JSON.parse(data.body)
+    /*  returnData.forEach(function(obj){
+        console.log(obj)
+      })*/
+      console.log(returnData);
+      $('body').html( show( returnData.cats[0] ) );
+    })
+  })
+
+
+
+
+
+// cats api :
