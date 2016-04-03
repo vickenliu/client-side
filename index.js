b63435d3fb793f9187bba246b1130ccb7612809e
mix irving <whimful@gmail.com>
2016-04-03 23:25:25 +1200

    working mvp hbs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var xhr = require('xhr')
+var example = require('./example.hbs')
 
-xhr.get('http://google.com', function(err, data) {
-  if (err) // do something
+xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+  if (err) console.log(err) // do something
 
-  console.log(data)
+  console.log(data.body)
+  document.body.innerHTML = example({ name: "Space" });
 })
 
