db1b40ef5873e31b71fdcaba51c802cf9b745bfd
vicken <vicken.liu@gmail.com>
2016-04-04 16:29:54 +1200

    Updates

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 var show = require('./views/show.hbs')
+
+//var showJade = require('./views/show.jade')
 var $ = require('jquery')
 
-xhr.get('http://localhost:3000/cats', function(err, data) {
-  if (err) console.log(err) // do something
-  var name = 'json',
-      returnData = JSON.parse(data.body)
-/*  returnData.forEach(function(obj){
-    console.log(obj)
-  })*/
-  console.log(returnData);
-  $('body').html( example( {cats: returnData.cats} ) );
-})
 
+function showAll(url){
+    xhr.get(url, function(err, data) {
+      if (err) console.log(err) // do something
+      var name = 'json',
+          returnData = JSON.parse(data.body)
+      $('body').html( example( {cats: returnData.cats} ) );
+    })
 
+}
+
+$(document).ready(function(){
+  showAll('http://localhost:3000/cats');
+  showSingle();
+  $('body').on('click','#showAll',function(e){
+    e.preventDefault();
+    showAll('http://localhost:3000/cats');
+  });
+});
+
+function showSingle(){
   $('body').on('click','.showCat',function(e){
     e.preventDefault();
     var cat = e.target,
@@ -24,13 +35,12 @@ xhr.get('http://localhost:3000/cats', function(err, data) {
       if (err) console.log(err) // do something
 
       var  returnData = JSON.parse(data.body)
-    /*  returnData.forEach(function(obj){
-        console.log(obj)
-      })*/
-      console.log(returnData);
       $('body').html( show( returnData.cats[0] ) );
     })
   })
+
+}
+
 
 
 
