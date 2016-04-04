1bdca74f0bfe461d2732271172b4e0731675cfc3
vicken <vicken.liu@gmail.com>
2016-04-04 12:03:31 +1200

    add a show all button

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ var xhr = require('xhr')
 var example = require('./views/example.hbs')
 var show = require('./views/show.hbs')
 var $ = require('jquery')
+function showAll(url){
+    xhr.get(url, function(err, data) {
+      if (err) console.log(err) // do something
+      var name = 'json',
+          returnData = JSON.parse(data.body)
+    /*  returnData.forEach(function(obj){
+        console.log(obj)
+      })*/
+      console.log(returnData);
+      $('body').html( example( {cats: returnData.cats} ) );
+    })
 
-xhr.get('http://localhost:3000/cats', function(err, data) {
-  if (err) console.log(err) // do something
-  var name = 'json',
-      returnData = JSON.parse(data.body)
-/*  returnData.forEach(function(obj){
-    console.log(obj)
-  })*/
-  console.log(returnData);
-  $('body').html( example( {cats: returnData.cats} ) );
-})
+}
 
+$(document).ready(function(){
+  showAll('http://localhost:3000/cats');
+  showSingle();
+  $('body').on('click','#showAll',function(e){
+    e.preventDefault();
+    showAll('http://localhost:3000/cats');
+  });
+});
 
+function showSingle(){
   $('body').on('click','.showCat',function(e){
     e.preventDefault();
     var cat = e.target,
@@ -31,6 +42,9 @@ xhr.get('http://localhost:3000/cats', function(err, data) {
       $('body').html( show( returnData.cats[0] ) );
     })
   })
+
+}
+
 
 
 
